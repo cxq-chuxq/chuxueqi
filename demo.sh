06d9b108abc9f878c84ae08d4416e2513aabfb9f
edz <edz@chuxq.local>
2020-01-11 20:46:34 +0800

    命令行练习

---
+++ demo.sh
@@ -0,0 +1,12 @@
+mkdir new
+touch new/new.txt
+echo "welcome to banyuan">new/new.txt
+mkdir newother
+mkdir newother/sub
+cp new.txt sub
+mv new/new.txt new/new1.txt
+mv sub practice
+mv practice/sub new2
+rm practice/new*
+
+
