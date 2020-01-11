58eafa3f98352be2acaf0b93902fbfcd37398742
edz <edz@chuxq.local>
2020-01-11 20:47:30 +0800

    git练习

---
+++ shell.sh
@@ -0,0 +1,12 @@
+mkdir example
+mv practice/ex01.c example
+mv example.c
+mkdir practice
+mv practice/ex02.c practice
+mv ex01.c
+remove ex03.c 
+mkdir README.md
+## example/example.c
+判断是奇数还是偶数
+## practice/parctive.c
+比较两个数大小
